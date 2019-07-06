dd27f016576ca95c81ff88dceb358d295c1ec79e
Bee <be04zimm@gmail.com>
2019-07-05 21:52:47 -0400

    images updates and some work on chosen-select code

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
-// var bodyParser = require('body-parser');
+//Dependencies:
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-// //Body Parser
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-// Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-//Router
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+//ROUTER
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
 
-//Listening to the port that was set up
-app.listen(port, () => console.log("Listening on port ", port));
+// Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
+});
