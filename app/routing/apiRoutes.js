2029748db78c2d0ff6c5989c69cf43f02a03be24
Bee <be04zimm@gmail.com>
2019-07-09 16:08:19 -0400

    testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
 var friends = require('../data/friends.js');
 module.exports = function (app) {
 
-    app.get('/api/friends', function (req, res) {
+    app.get('/app/data/friends', function (req, res) {
         res.json(friends);
     });
 
-    app.post('/api/friends', function (req, res) {
+    app.post('/app/data/friends', function (req, res) {
 
         var totalDifference = 0;
         var bestMatch = {
