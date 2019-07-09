47748adf0c9fa9a79d51a58685c7de43ba38a554
Bee <be04zimm@gmail.com>
2019-07-09 15:35:04 -0400

    friend score changes and removing Path method

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,37 @@
 var friends = [{
     "name": "Betty Boop",
     "photo": "./public/img/Betty.png",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "4"]
 },
 {
     "name": "Thomas the Train",
     "photo": "./public/img/Thomas.jpeg",
-    "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    "scores": ["2", "1", "5", "2", "4", "3", "2", "1", "2", "4"]
 },
 {
     "name": "The Mighty Bob",
     "photo": "./public/img/Bob.jpg",
-    "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    "scores": ["4", "3", "5", "1", "2", "4", "1", "5", "5", "5"]
 },
 {
     "name": "Paula Poundstone",
     "photo": "./public/img/Paula.jpeg",
-    "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    "scores": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
 },
 {
     "name": "Lily Tiger",
     "photo": "./public/img/Lily.jpeg",
-    "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    "scores": ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
 },
 {
     "name": "Famous Fred",
     "photo": "./public/img/Fred.jpg",
-    "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+    "scores": ["3", "5", "5", "5", "2", "5", "1", "3", "4", "1"]
 },
 {
     "name": "George George",
     "photo": "./public/img/George.jpeg",
-    "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    "scores": ["3", "3", "2", "1", "1", "4", "2", "4", "4", "5"]
 }
 
 ]
