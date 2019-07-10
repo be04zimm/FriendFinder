c590554f6339d79eafcdfa0c9f835fb52c97b9ea
Bee <be04zimm@gmail.com>
2019-07-10 16:50:35 -0400

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,108 @@
-var friends = [{
-    "name": "Betty Boop",
-    "photo": "./public/img/Betty.png",
-    "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "4"]
-},
-{
-    "name": "Thomas the Train",
-    "photo": "./public/img/Thomas.jpeg",
-    "scores": ["2", "1", "5", "2", "4", "3", "2", "1", "2", "4"]
-},
-{
-    "name": "The Mighty Bob",
-    "photo": "./public/img/Bob.jpg",
-    "scores": ["4", "3", "5", "1", "2", "4", "1", "5", "5", "5"]
-},
-{
-    "name": "Paula Poundstone",
-    "photo": "./public/img/Paula.jpeg",
-    "scores": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-},
-{
-    "name": "Lily Tiger",
-    "photo": "./public/img/Lily.jpeg",
-    "scores": ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
-},
-{
-    "name": "Famous Fred",
-    "photo": "./public/img/Fred.jpg",
-    "scores": ["3", "5", "5", "5", "2", "5", "1", "3", "4", "1"]
-},
-{
-    "name": "George George",
-    "photo": "./public/img/George.jpeg",
-    "scores": ["3", "3", "2", "1", "1", "4", "2", "4", "4", "5"]
-}
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
 
-]
 
+var friends = [
+  {
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Jacob Deming",
+    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Jeremiah Scanlon",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Louis T. Delia",
+    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Lou Ritter",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Jordan Biason",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
 module.exports = friends;
--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-//Dependencies:
-var express = require('express');
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(express.json());
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//ROUTER
-require('./app/routing/api-routes.js')(app);
-require('./app/routing/html-routes.js')(app);
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-// Starts the server to begin listening
-app.listen(PORT, function () {
-    console.log('App listening on PORT: ' + PORT);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
